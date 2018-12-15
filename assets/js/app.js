36a85a8d92936721deb42aa486da9bf9f13ddc0e
NamaN <nammu.neema@gmail.com>
2018-12-15 14:53:46 +0530

    fix navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,39 +4,53 @@ $(document).ready( ()=> {
         console.log('callback - particles.js config loaded');
       });
 
-      $(document).on('click', 'a[href^="#"]', function (event) {
+      let responsiveMaker = () => {
+        if ($(window).innerWidth() <= 576) {
+            $('#header').addClass('header-display');
+            $('#menu-btn').removeClass('header-display');
+            $('#menu-btn').on('click', (e) => { 
+                e.preventDefault();
+                $('#header').toggleClass('responsive-header');
+            });
+        } else {
+            $('#header').removeClass('responsive-header header-display');
+            $('#menu-btn').addClass('header-display');
+            
+        }
+    }
+
+    responsiveMaker();
+
+
+      $(document).on('click', `a[href^='#']`, function (event) {
         event.preventDefault();
-    
         $('html, body').animate({
             scrollTop: ($($.attr(this, 'href')).offset().top - 75) 
         }, 500);
     });
-    $('.custom-progress-bar-container').each(function(){
+    $('.custom-progress-bar-container').each(function() {
         let bar = $(this).find('.custom-progress-bar')
         bar.animate({
-			width:bar.attr("data-percent")
-		},1000);
+			width:bar.attr('data-percent')
+		}, 1000);
 	});
       $(window).scroll((event) => {
-        var scroll = $(window).scrollTop();
-        // Do something
-        var totalHeight = $(window).innerHeight();
-        var activeHeight = totalHeight * .80;
-        if (scroll >= activeHeight ){
-        //   $("#nav-bar").fadeIn(500);
-        $("#nav-bar").removeClass("custom-nav-hidden");  
+        const scroll = $(window).scrollTop();
+        const totalHeight = $(window).innerHeight();
+        const activeHeight = totalHeight * .80;
+        if (scroll >= activeHeight) {
+        $('#nav-bar').removeClass('custom-nav-hidden');  
+        } else{ 
+            $('#nav-bar').addClass('custom-nav-hidden');
         }
-        else{
-            // $("#nav-bar").fadeOut(500);  
-            $("#nav-bar").addClass("custom-nav-hidden");
-        }
-        
-        
     });
-
     $('.js-tilt').tilt({
-       
         glare: true,
         maxGlare: .2
+    });
+
+    
+    $(window).resize(() => { 
+        responsiveMaker()
     });
 })
